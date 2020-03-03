64a4938b9a7ac2f0b73acd39f4aeb0c0f981b399
momodupi <momodupi@gmail.com>
2020-03-03 18:08:10 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+
 
 def main():
+    conf_path = 'conf'
+    result_path = 'result'
+    try:
+        os.mkdir(conf_path)
+        os.mkdir(result_path)
+    except OSError:
+        #print ("Creation of the directory %s failed")
+        pass
+
     token = open('conf/.mapbox_token', 'w')
     token_key = input('Paste your Mapbox public token and press Enter: ')
     token.write(token_key)
